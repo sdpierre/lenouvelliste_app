ebf4bcf6aedc04a61da50ed2c3bf605498297bd4
Stanley Dany Pierre <stanleydanypierre@gmail.com>
2020-02-28 13:13:41 -0500

    icon, splash

--- src/library/networking/Api.js
+++ src/library/networking/Api.js
@@ -7,3 +7,8 @@ export async function getBreakingNews() {
     let result = await fetch("http://lenouvelis.com/api/homenews").then(response => response.json());
     return result.data;
   }
+
+  export async function getAllNews() {
+    let result = await fetch("http://lenouvelis.com/api/getallnews").then(response => response.json());
+    return result.data;
+  }
